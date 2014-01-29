ee094cb28255c7b8ec4219cc97ae23e4472a28ff
ChrisNeveu <cneveu@gawker.com>
2014-01-29 11:53:01 -0500

    Updating soy-plugins dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.SbtScalariform._
 object ApplicationBuild extends Build {
 
   val appName         = "play2-closure"
-  val appVersion      = "0.26-2.2.1-" + {if (System.getProperty("JENKINS_BUILD") == null) "SNAPSHOT" else "RELEASE"}
+  val appVersion      = "0.27-2.2.1-" + {if (System.getProperty("JENKINS_BUILD") == null) "SNAPSHOT" else "RELEASE"}
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
   val localSettings = scalariformSettings ++ Seq(
     // Add your own project settings here
     libraryDependencies ++= Seq(("com.google.template" % "soy" % "2012-12-21").exclude("asm", "asm"),
-								"soy-plugins" %% "soy-plugins" % "0.4.5-RELEASE"),
+								"soy-plugins" %% "soy-plugins" % "0.4.6-RELEASE"),
     resolvers += "Gawker Public Group" at "https://nexus.kinja-ops.com/nexus/content/groups/public/",
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     scalaVersion := "2.10.2",
